65bec8db00108d4ba13a5f9297d935de346349d6
tumenbaevaj <tumenbaevaj@gmail.com>
2026-03-30 11:57:43 +0600

    getMessage() method was added

--- src/main/java/com/gmail/tumenbaevaj/Main.java
+++ src/main/java/com/gmail/tumenbaevaj/Main.java
@@ -2,6 +2,10 @@ package com.gmail.tumenbaevaj;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello from QA");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello from QA";
     }
 }
