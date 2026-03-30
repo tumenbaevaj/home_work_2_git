95d7a65daf707f3206989c6d2d003f937755e840
Jibek Tumenbaeva <tumenbaevaj@gmail.com>
2026-03-30 12:13:54 +0600

    Update Main.java

--- src/main/java/com/gmail/tumenbaevaj/Main.java
+++ src/main/java/com/gmail/tumenbaevaj/Main.java
@@ -2,7 +2,7 @@ package com.gmail.tumenbaevaj;
 
 public class Main {
 
-    private static String message = "Hello from QA!!!!";
+    private static String message = "Hello qa.guru!!!!";
     
     public static void main(String[] args) {
         System.out.println(message);
